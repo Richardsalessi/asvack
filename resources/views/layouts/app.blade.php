656de8704edade6fe8f5a787dbe09566ba99681f
Richards <richardsagonzalezp21@gmail.com>
2024-10-26 11:10:41 -0500

    Actualización de app.blade.php para arreglar el comportamiento del botón de scroll

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,4 @@
+<!--app.blade.php -->
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ darkMode: localStorage.getItem('darkMode') === 'true' }" x-init="
     if (darkMode) { 
